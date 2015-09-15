3b31a3e1c9829812ef80a534f6efff6e65a031e9
Michael Dorrance <dorrancemd@gmail.com>
2015-09-14 18:01:50 -0600

    added remaining find functionality for controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::ItemsController < ApplicationController
   def show
     respond_with Item.find_by(id: params[:id])
   end
+
+  def find
+    if params.include?("name")
+      respond_with Item.find_by(name: params[:name])
+    elsif params.include?("description")
+      respond_with Item.find_by(description: params[:description])
+    elsif params.include?("merchant_id")
+      respond_with Item.find_by(merchant_id: params[:merchant_id])
+    elsif params.include?("unit_price")
+      respond_with Item.find_by(unit_price: params[:unit_price])
+    else
+      respond_with Item.find_by(id: params[:id])
+    end
+  end
 end
